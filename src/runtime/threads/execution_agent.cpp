006cfa6106bd109c4bf5655f8733549799f3c44c
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2019-10-15 08:39:34 -0500

    Fixing inspect issues

--- src/runtime/threads/execution_agent.cpp
+++ src/runtime/threads/execution_agent.cpp
@@ -1,9 +1,11 @@
 //  Copyright (c) 2019 Thomas Heller
 //
+//  SPDX-License-Identifier: BSL-1.0
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <hpx/config.hpp>
+#include <hpx/assertion.hpp>
 #include <hpx/errors/throw_exception.hpp>
 #include <hpx/format.hpp>
 #include <hpx/logging.hpp>
@@ -22,7 +24,9 @@
 
 #include <hpx/util/thread_description.hpp>
 
+#include <cstddef>
 #include <sstream>
+#include <string>
 
 namespace hpx { namespace threads {
 
